97c42c13feced7cdb46724a301ec91c922aae3b5
Konstantin <rudnevaketi@gmail.com>
2023-03-14 16:38:15 +0300

    fix import

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ import WordCardSlider from './components/WordCardSlider/WordCardSlider';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <WordCardSlider cards={cards} />
-      {
-        words.map((word) =>
-          <WordsList key={word.id} {...word} />
-        )
-      }
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<WordCardSlider cards={cards} />
+			{words.map((word) => (
+				<WordsList key={word.id} {...word} />
+			))}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,8 +8,8 @@ function Header() {
             <h1 className='header__title'>English Words</h1>
             <nav>
                 <ul className="header__list">
-                    <li className="header__item"><a href="#" className="header__link">Слова</a></li>
-                    <li className="header__item"><a href="#" className="header__link">Игра</a></li>
+                    <li className="header__item"><a href="./" className="header__link">Слова</a></li>
+                    <li className="header__item"><a href="./" className="header__link">Игра</a></li>
                 </ul>
             </nav>
         </header>
