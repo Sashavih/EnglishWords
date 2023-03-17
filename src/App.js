25e9d027b17800490301083e31d75116d4220ae1
Sasha Sasha <sashamaninaa@mail.ru>
2023-03-17 11:35:32 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import './components/variables.scss';
 import words from './utils/words';
 import cards from './utils/cards';
 import Header from './components/Header/Header';
@@ -8,16 +7,16 @@ import WordCardSlider from './components/WordCardSlider/WordCardSlider';
 import Footer from './components/Footer/Footer';
 
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<WordCardSlider cards={cards} />
-			{words.map((word) => (
-				<WordsList key={word.id} {...word} />
-			))}
-			<Footer />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Header />
+      <WordCardSlider cards={cards} />
+      {words.map((word) => (
+        <WordsList key={word.id} {...word} />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
