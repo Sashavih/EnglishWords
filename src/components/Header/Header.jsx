49717fb458c89e8fe3d1bc0138609ce69880a8cd
Sasha Sasha <sashamaninaa@mail.ru>
2023-04-22 17:41:26 +0300

    add new meta

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <header className="header">
             <Link to={"/"}> <img src={logo} alt="logo" className="header__logo" /></Link>
             <h1 className='header__title'>English Words</h1>
-            <nav>
+            <nav className='header__nav'>
                 <ul className="header__list">
                     <li className="header__item"><Link to={"/"} className="header__link">Main</Link></li>
                     <li className="header__item"><Link to={"/Game"} className="header__link">Game</Link></li>
